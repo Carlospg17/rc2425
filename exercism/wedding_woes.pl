fdcf19fdc890670a13efaf4338ea2b60e8064d74
Carlospg17 <carlos.perez1@alu.uhu.es>
2025-02-27 09:30:35 +0100

    Update wedding_woes.pl

--- exercism/wedding_woes.pl
+++ exercism/wedding_woes.pl
@@ -1,1 +1,15 @@
+% Define the 'chatty' fact
+chatty(gustavo).
+chatty(valeria).
 
+% Define the 'likes' fact
+likes(esteban, malena).
+likes(malena, esteban).
+likes(gustavo, valeria).
+
+% Define the 'pairing' rule
+pairing(A, _):- chatty(A).
+pairing(_, A):- chatty(A).
+pairing(A, B):- likes(A, B), likes(B, A).
+
+% Define the 'seating' rule
