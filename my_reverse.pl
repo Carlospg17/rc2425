9fe7efb8dd6870bb13d9c1a5d6138d33e8feb1fc
Carlospg17 <carlos.perez1@alu.uhu.es>
2025-03-20 09:52:29 +0100

    Add files via upload

---
+++ my_reverse.pl
@@ -0,0 +1,7 @@
+my_reverse([], []).
+my_reverse([Cab|Resto],R2) :- my_reverse(Resto,R), 
+	append(R, [Cab], R2).
+	
+
+my_append([],List,List).
+my_append([Cab|Resto],List, [Cab|R]):- my_append(Resto, List, R).
