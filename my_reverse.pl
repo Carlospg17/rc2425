fd1474015b6d011a28e53b0ca9fabe94dafd68dd
Carlospg17 <carlos.perez1@alu.uhu.es>
2025-03-20 09:53:35 +0100

    Update my_reverse.pl

--- my_reverse.pl
+++ my_reverse.pl
@@ -1,6 +1,5 @@
 my_reverse([], []).
-my_reverse([Cab|Resto],R2) :- my_reverse(Resto,R), 
-	append(R, [Cab], R2).
+my_reverse([Cab|Resto],R2) :- my_reverse(Resto,R), my_append(R, [Cab], R2).
 	
 
 my_append([],List,List).
